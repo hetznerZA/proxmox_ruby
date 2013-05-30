da2aad061af767b0f1448dafa8dde8bf24a43ed4
Rory McKinley <rorymckinley@gmail.com>
2013-05-30 22:20:12 +0200

    Rough and ready creation of VM

--- check_for_a_pulse.rb
+++ check_for_a_pulse.rb
@@ -16,12 +16,13 @@ resp =  connection.post("/api2/json/access/ticket") do |req|
 end
 
 data = JSON.parse(resp.body, symbolize_names: true)
-p data[:data]
 ticket = data[:data][:ticket]
+csrfp_prevention_token =  data[:data][:CSRFPreventionToken]
 
 connection = Faraday.new(url: "https://#{servername}:8006", ssl: {verify: false}) do |conn|
   conn.request  :url_encoded
   conn.headers["Cookie"] = "PVEAuthCookie=#{ticket}"
+  conn.headers["CSRFPreventionToken"] = csrfp_prevention_token
   conn.adapter  Faraday.default_adapter
 end
 
@@ -31,11 +32,36 @@ nodes = JSON.parse(resp.body, symbolize_names: true)[:data]
 
 node = nodes.first
 
-p node[:mem].to_f / node[:maxmem].to_f
-p node[:disk].to_f / node[:maxdisk].to_f
+# p nodes
+#
+# p node[:mem].to_f / node[:maxmem].to_f
+# p node[:disk].to_f / node[:maxdisk].to_f
+#
+# p node[:node]
 
 resp = connection.get("/api2/json/nodes/#{node[:node]}/qemu")
 
-qemus = JSON.parse(resp.body, symbolize_names: true)
+qemus = JSON.parse(resp.body, symbolize_names: true)[:data]
 
-p qemus
+inuse_vmids = qemus.map { |vm| vm[:vmid].to_i }
+
+p inuse_vmids
+
+next_vmid = nil
+
+until next_vmid do
+  candidate_vmid = 101 + Random.rand(200)
+  unless inuse_vmids.include? candidate_vmid
+    next_vmid = candidate_vmid
+    inuse_vmids << candidate_vmid
+  end
+end
+
+p next_vmid
+resp = connection.post("/api2/json/nodes/#{node[:node]}/qemu") do |req|
+  req.body = { vmid: next_vmid, name: "rory.is.awesome", onboot: 1, ostype: "l26", net0: "virtio,bridge=vmbr0", virtio0: "VM_USE:12,cache=writeback" }
+  # onboot - start at boot
+  # VM_USE:10 trick - found here http://forum.proxmox.com/threads/12059-API-Create-KVM-with-Logical-Volume
+end
+
+p resp.body
